f0dc9cc0ca90d2ccef5eb8d4b67cc7f5f28b79bc
Felipe Santiago <you@example.com>
2020-03-31 01:33:54 -0300

    Ajuste knexfile

--- src/database/knexfile.js
+++ src/database/knexfile.js
@@ -2,13 +2,13 @@ require('../bootstrap');
 
 module.exports = {
   development: {
-    client: process.env.DB_DIALECT || 'sqlite3',
+    client: process.env.DB_DIALECT,
     connection: {
       host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      filename: process.env.STORAGE || './src/database/db.sqlite',
+      filename: process.env.STORAGE,
     },
     pool: {
       min: 2,
@@ -19,5 +19,18 @@ module.exports = {
       directory: '../database/migrations',
     },
     useNullAsDefault: true,
+  },
+
+  production: {
+    client: process.env.DB_DIALECT,
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'migrations',
+      directory: './migrations',
+    },
   },
 };
