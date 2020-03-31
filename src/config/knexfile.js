979d238416fd5693cbd65f5abf7e3ac7b7922272
Felipe Santiago <you@example.com>
2020-03-31 01:05:00 -0300

    Configura Heroku

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -3,13 +3,14 @@ require('../bootstrap');
 module.exports = {
   development: {
     client: process.env.DB_DIALECT,
-    connection: {
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      filename: process.env.STORAGE,
-      ssl: true,
-    },
+    connection: process.env.DATABASE_URL,
+    // connection: {
+    //   database: process.env.DB_NAME,
+    //   user: process.env.DB_USER,
+    //   password: process.env.DB_PASS,
+    //   filename: process.env.STORAGE,
+    //   ssl: true,
+    // },
     pool: {
       min: 2,
       max: 10,
