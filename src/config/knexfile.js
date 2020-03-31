cad8377602b9cab36e95d6d47c26ef3f96a2bc18
Felipe Santiago <you@example.com>
2020-03-31 01:12:12 -0300

    Configura Heroku

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -21,4 +21,25 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    // connection: {
+    //   database: process.env.DB_NAME,
+    //   user: process.env.DB_USER,
+    //   password: process.env.DB_PASS,
+    //   filename: process.env.STORAGE,
+    //   ssl: true,
+    // },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      // tableName: 'migrations',
+      directory: '../database/migrations',
+    },
+    useNullAsDefault: true,
+  },
 };
