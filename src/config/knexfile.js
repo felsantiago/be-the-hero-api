068069253572184949eeece57a3deeef2a5d29b1
Felipe Santiago <you@example.com>
2020-03-31 01:21:35 -0300

    Configura Heroku

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -2,44 +2,33 @@ require('../bootstrap');
 
 module.exports = {
   development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    // connection: {
-    //   database: process.env.DB_NAME,
-    //   user: process.env.DB_USER,
-    //   password: process.env.DB_PASS,
-    //   filename: process.env.STORAGE,
-    //   ssl: true,
-    // },
+    client: process.env.DB_DIALECT,
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      filename: process.env.STORAGE,
+    },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      // tableName: 'migrations',
+      tableName: 'migrations',
       directory: '../database/migrations',
     },
     useNullAsDefault: true,
   },
 
   production: {
-    client: 'pg',
+    client: process.env.DB_DIALECT,
     connection: process.env.DATABASE_URL,
-    // connection: {
-    //   database: process.env.DB_NAME,
-    //   user: process.env.DB_USER,
-    //   password: process.env.DB_PASS,
-    //   filename: process.env.STORAGE,
-    //   ssl: true,
-    // },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      // tableName: 'migrations',
-      directory: '../database/migrations',
+      tableName: 'migrations',
     },
-    useNullAsDefault: true,
   },
 };
